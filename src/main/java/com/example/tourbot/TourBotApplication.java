603692a10785f43a4acc7cb03099a938ef016ec8
narminya <narminya@code.edu.az>
2024-02-22 22:59:52 +0400

    Initial project creation

---
+++ src/main/java/com/example/tourbot/TourBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourBotApplication.class, args);
+    }
+
+}
