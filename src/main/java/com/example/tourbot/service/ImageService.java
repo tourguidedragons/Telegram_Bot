ca13f90895ed47c982ed075d924a74d67289e2c2
narminya <narminya@code.edu.az>
2024-02-28 00:17:15 +0400

    feat : enable image sending
    This commit enables sending images to telegram bot using AbsSender

---
+++ src/main/java/com/example/tourbot/service/ImageService.java
@@ -0,0 +1,39 @@
+package com.example.tourbot.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.DefaultAbsSender;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.File;
+
+@Service
+@Slf4j
+public class ImageService extends DefaultAbsSender {
+    public ImageService(@Value("${telegrambot.botToken}") String telegramBotToken) {
+        super(new DefaultBotOptions(), telegramBotToken);
+    }
+
+    public void sendPhotoToChat(Long chatId, String photoFilePath, String caption, ReplyKeyboard keyboard) {
+        try {
+
+            SendPhoto sendPhoto = new SendPhoto();
+            sendPhoto.setChatId(chatId);
+            InputFile inputFile = new InputFile();
+            inputFile.setMedia(new File(photoFilePath));
+            sendPhoto.setPhoto(inputFile);
+            sendPhoto.setCaption(caption);
+            sendPhoto.setReplyMarkup(keyboard);
+            this.execute(sendPhoto);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
