5a25ce440dbac06ad1c671256c10ea516147aea3
narminya <narminya@code.edu.az>
2024-03-01 02:18:28 +0400

    Update SessionRepository.java

--- src/main/java/com/example/tourbot/repository/SessionRepository.java
+++ src/main/java/com/example/tourbot/repository/SessionRepository.java
@@ -7,10 +7,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface SessionRepository extends JpaRepository<Session, Integer> {
     List<Session> findByClientId(Long clientId);
 
     @Query(value = "UPDATE requests SET is_active = FALSE WHERE expire_date < now() AND  expire_date IS NOT NULL AND is_active = TRUE", nativeQuery = true)
     void expireSessions();
+
+    Session findByuuid(UUID uuid);
 }
